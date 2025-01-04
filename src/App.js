f08ba812cafcca10463feeaed0046a94c559a613
Alok3333 <alokprajapati697@gmail.com>
2025-01-04 11:08:12 +0530

    x classcom completed

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Xcounter from "./Xcounter/Xcounter";
+
+function App() {
+  return (
+    <div className="App">
+      <Xcounter />
+    </div>
+  );
+}
+
+export default App;
